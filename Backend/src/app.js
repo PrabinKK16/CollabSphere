e7c449e6c70b096b8c7beb494d97230009c0849c
PrabinKK16 <prabinkumarkhamania7@gmail.com>
2026-02-11 12:27:35 +0530

    update 2 files and create 6 files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use(errorHandler);
 
 export default app;
--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -28,6 +28,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      select: false, 
     },
     avatar: {
       type: String,
@@ -40,11 +41,12 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-userSchema.methods.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function(password) {
   return await bcrypt.compare(password, this.password);
-};
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
